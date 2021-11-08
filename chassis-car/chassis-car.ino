a4f8b114f1e5da6e34624e71dc80b20acd650ec7
alpiha <alpiha1515@gmail.com>
2021-11-08 16:33:06 -0500

    add: change state when route is finished

--- chassis-car/chassis-car.ino
+++ chassis-car/chassis-car.ino
@@ -16,24 +16,28 @@ int motorRightSpeed = 11;       //PWM B
 
 // states 
 enum Modes {INPUTMODE, ACTIVEMODE, PAUSEMODE};
-Modes currentMode = INPUTMODE;
+Modes currentMode = ACTIVEMODE;
 
 // moving directions 
 // F: forward, R: Right, L: Left, B: Backwads
 enum Direction {F, R, L, B};
 
+//current direction
 int i = 0;
 
 // Test Routes 
-int routeF[] = {F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F};
-int route[] = {F, R, F, F, L, B, B, R, B};
+Direction routeF[] = {F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F};
+Direction route[] = {F, R, F, F, L, B, B, R, B};
+
+// atm debug
+boolean pausemodePrinted = false;
 
 
 void setup() {
   //Declares that the pin is being used for detecting inputs, while activating the pins build in pull-up resistor
   pinMode(rWheel, INPUT_PULLUP);
   pinMode(lWheel, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(rWheel), count, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(rWheel), countR, CHANGE);
   attachInterrupt(digitalPinToInterrupt(lWheel), countL, CHANGE);
 
   //Motor A (Left)
@@ -49,47 +53,49 @@ void setup() {
 }
 
 void loop() {
-  switch (currentMode) {
+    switch (currentMode) {   
     case INPUTMODE: //Forward
-      Serial.println("In inputmode");
-      compare(counterL, counterR);
+      Serial.println("currentMode is:");
+      Serial.println(currentMode);
       //TODO: Listen for inputs
       //If statement - play is pressed = change mode to activemode
       break;
     case ACTIVEMODE: //Right
+      Serial.println("currentMode is:");
+      Serial.println(currentMode);
+      compare(counterL, counterR);
+      
+      if ((sizeof(routeF)/2) < i ){ // See and change mode if route is finished. 
+        Serial.println("currentMode is:");
+        Serial.println(currentMode);
+        //Serial.println("the route is finished, changed mode to: pause mode");
+        currentMode = PAUSEMODE;
+      }
       break;
       
       //TODO: Run the given route
       //If statement - route is finished = change mode to inputmode
       //If statement - pause is pressed = change mode to pausemode
-
-      //Set counters to 0
       
     case PAUSEMODE: //Left
-      Serial.println("In pausemode");
+      if (pausemodePrinted == false){
+        Serial.println("In pausemode");
+        pausemodePrinted = true;
+      }
+      //TODO: set counters to 0
+      //resetCounters()
       break;
-      
       //TODO: Wait for input
       //If statement - if play is pressed = change mode to activemode
       //If statement - if stop is pressed = change mode to inputmode
-    default:
-      Serial.println("Ukendt mode");
+    default: // If 
+      Serial.println("Unknown mode, the mode is changed to: pausemode");
+      currentMode = PAUSEMODE;
   }
 }
-
-//Fires when the used pin changes from HIGH->LOW or LOW->HIGH
-void countR() {
-  counterR++;
-}
-
-void countL() {
-  counterL++;
-}
-
 //Drives the robot
 void drive(boolean leftDirection, int leftSpeed, boolean rightDirection, int rightSpeed, int distance) {
-  counterR = 0;
-  counterL = 0;
+  resetCounters();
   
   //Motor A (Left)
   digitalWrite(motorLeftDirection, leftDirection);
@@ -106,7 +112,7 @@ void drive(boolean leftDirection, int leftSpeed, boolean rightDirection, int rig
 void routing(int leftSpeed, int rightSpeed){
   int j = 0;
   //Beware that the usage Serial can disturb interrupt services
-  switch (route[0]) {
+  switch (routeF[0]) {
     case F: //Forward
       while (j < 20) {
         drive(HIGH, leftSpeed, HIGH, rightSpeed, 5);
@@ -143,10 +149,7 @@ void routing(int leftSpeed, int rightSpeed){
   
 }
 void compare(int counterL, int counterR){
-  Serial.println("counterL");
-  Serial.println(counterL);
-  Serial.println("counterR");
-  Serial.println(counterR);
+  //debugC();
   if (counterR == counterL){
     routing(255, 255);
   } else if (counterR > counterL){
@@ -155,4 +158,24 @@ void compare(int counterL, int counterR){
     routing(255-(counterR - counterL), 255);
   }
   
+}
+
+void debugC(){ //debug counters
+  Serial.println("counterL");
+  Serial.println(counterL);
+  Serial.println("counterR");
+  Serial.println(counterR);
+}
+
+void resetCounters(){
+  counterR = 0;
+  counterL = 0;
+}
+
+//Fires when the used pin changes from HIGH->LOW or LOW->HIGH
+void countR() {
+  counterR++;
+}
+void countL() {
+  counterL++;
 }
