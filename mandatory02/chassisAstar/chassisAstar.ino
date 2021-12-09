c0ca723196ad619343efa2384b582db378241776
joakimbetzer <joakim.betzer@hotmail.com>
2021-12-09 12:09:50 +0100

    It kinda works

--- mandatory02/chassisAstar/chassisAstar.ino
+++ mandatory02/chassisAstar/chassisAstar.ino
@@ -1,13 +1,13 @@
 //Motor A (Left)
 int motorLeftDirection = 12;   //DIR A
-int motorLeftSpeed = 6;        //PWM A
+int motorLeftSpeed = 3;        //PWM A
 
 //Motor B (Right)
 int motorRightDirection = 13;   //DIR B
 int motorRightSpeed = 11;       //PWM B
 
-int sensorR = A3;
-int sensorL = A2;
+int sensorR = A2;
+int sensorL = A3;
 
 int sensorRight;
 int sensorLeft;
@@ -15,6 +15,11 @@ int sensorLeft;
 int echo = A4;
 int trig = A5;
 int distance;
+
+int lightLevel = 600;
+int maxSpeed = 225;
+int minSpeed = 0;
+int normd = 5;
 
 //ShiftRegister Pins
 int latchPin = 9; //ST_CP or RCLK ()
@@ -24,10 +29,10 @@ int dataPin = 10; //DS or SER ()
 //Command for route
 int progress = 0;
 
-char route [] = {'L', 'R'};
+char route [] = {'L', 'R', 'F'};
 
-enum Modes {FORWARDMODE, LEFTMODE, RIGHTMODE};
-Modes currentMode = FORWARDMODE;
+enum Modes {WOBBLEMODE, FORWARDMODE, LEFTMODE, RIGHTMODE, ENDOFROUTE};
+Modes currentMode = WOBBLEMODE;
 
 boolean hasStopped = false;
 boolean b1 = false;
@@ -64,94 +69,105 @@ void setup() {
 }
 
 void loop() {
-  sensorRight = analogRead(sensorR);
-  sensorLeft = analogRead(sensorL);
-/*
-  Serial.println("right sensor:");
+  readSensors();
+  /*
+  Serial.println("sensorRight:");
   Serial.println(sensorRight);
-  Serial.println("left sensor:");
+  Serial.println("sensorLeft:");
   Serial.println(sensorLeft);
-*/
-  if (sensorRight >= 700 && sensorLeft >= 700){
-    if(!hasStopped){
-      drive(LOW, 0, LOW, 0, 100);
-      hasStopped = true;
-    }
-    currentMode = LEFTMODE;
-    Serial.println("CHANGING TO LEFTMODE");
-    /*
-    Serial.println("SLTOOTTO BOTH");
-    switch (route[progress]){
-      case 'F':
-        drive(LOW, 255, LOW, 255, 5);
-        Serial.println("FORWARDDDDD");
-        break;
-      case 'L':
-        drive(LOW, 255, LOW, 0, 5);
-        Serial.println("LEEFTTTTTTT");
-        break;
-      case 'R':
-        drive(LOW, 0, LOW, 255, 5);
-        Serial.println("RGHTHTHTHTHTHTH");
-        break;
-      default:
-        drive(LOW, 0, LOW, 0, 5);
-        Serial.println("Default mode called");
-    }
-    */
-  } 
-  if (sensorRight >= 700 && currentMode == LEFTMODE){
-      b1 = true;
-      Serial.println("b1 true");
-  }
-  if (sensorRight <= 700 && currentMode == LEFTMODE && b1){
-      b2 = true; 
-      Serial.println("b2 true");
-  }
-
-
-  if (sensorLeft >= 700 && currentMode == RIGHTMODE){
-      b1 = true;
-      Serial.println("b1 true");
-  }
-  if (sensorLeft <= 700 && currentMode == RIGHTMODE && b1){
-      b2 = true; 
-      Serial.println("b2 true");
-  }
+  */
+  writeShiftRegister(B00000000);
+  Serial.println(getMode());
+  //turnOnLED();
   
-  if (b1 && b2){
-      currentMode = FORWARDMODE;
-      hasStopped = false;
-      Serial.println("FORWARD MODE CHANGING TO");
-      b1 = false;
-      b2 = false;
-  }
-
-  switch (currentMode){
-    case FORWARDMODE:
-      if(sensorRight <= 700 && sensorLeft <= 700){
-        drive(LOW, 225, LOW, 225, 5);
-      }else if(sensorRight <= 700){
-      drive(LOW, 225, LOW, 0, 5);
+  checkMode();
+  
+  //When the sensors both are black
+  if (currentMode == WOBBLEMODE){
+    Serial.println("HHHHHHHHHH");
+    if (sensorRight <= lightLevel && sensorLeft <= lightLevel){
+      drive(LOW, maxSpeed, LOW, maxSpeed, normd);
+      
+    }else if(sensorRight >= lightLevel){
+      drive(LOW, 0, LOW, maxSpeed, normd);
       Serial.println("sluk right");
       
-      } else if(sensorLeft <= 700){
-      drive(LOW, 0, LOW, 225, 5);
+    }else if(sensorLeft >= lightLevel){
+      drive(LOW, maxSpeed, LOW, 0, normd);
       Serial.println("sluk leftt");
+    }
+    
+  }else if (currentMode == LEFTMODE){
+    while(currentMode == LEFTMODE){
+      Serial.println("LEFT PRINT");
+      drive(LOW, maxSpeed, LOW, 0, normd);
+      checkIfTurned(sensorLeft);
+    }
+    
+  }else if (currentMode == RIGHTMODE){
+    while(currentMode == RIGHTMODE){
+      Serial.println("RIGHT PRINT");
+      drive(LOW, 0, LOW, maxSpeed, normd);
+      checkIfTurned(sensorRight);
+  }
+  
+  }else if (currentMode == FORWARDMODE){
+    while(currentMode == FORWARDMODE){
+      Serial.println("FORAWRD PRINT");
+      drive(LOW, maxSpeed, LOW, maxSpeed, normd);
+      checkIfForward();
+    }
+  }
+}
+
+void checkIfTurned(int sensorU){
+    while (!b2){
+      Serial.println("B2 CHECKIFTURNED PRINT");
+      int sensor = readSensors(sensorU);
+      sensorDebug();
+        if (sensor <= lightLevel){
+          b1 = true;
+          Serial.println("b1 trueeeee");
+        }    
+        if (sensor >= lightLevel && b1){
+          progress++;
+          Serial.println("progress ++");
+          currentMode = WOBBLEMODE;
+          b2 = true;
+        }
       }
-      break;
-
-    case LEFTMODE:
-      drive(LOW, 200, LOW, 0, 20);
-      Serial.println("Turning leftLLL");
-      break;
-
-    case RIGHTMODE:
-      drive(LOW, 0, LOW, 255, 20);
-      Serial.println("Turning righththththth");
-      break;
+      b1 = false;
+      b2 = false;
 }
+
+void checkIfForward(){
+  if (sensorRight <= lightLevel && sensorLeft <= lightLevel){
+    currentMode = WOBBLEMODE;
+  }
 }
+
+void checkMode(){
+  if (sensorRight >= lightLevel && sensorLeft >= lightLevel){
+    switch(route[progress]){
+      case 'F':
+        currentMode = FORWARDMODE;
+        break;
+
+      case 'L':
+        currentMode = LEFTMODE;
+        break;
+
+      case 'R':
+        currentMode = RIGHTMODE;
+        break;
+
+      default:
+        drive(LOW, 0, LOW, 0, normd);
+        Serial.println("Route has ended, thanks for joining us");
+    }
+  }
+}
+
 //Drives the robot
 void drive(boolean leftDirection, int leftSpeed, boolean rightDirection, int rightSpeed, int distance) {
   if (checkObstacle()){
@@ -188,6 +204,25 @@ void writeShiftRegister(int output) {
   digitalWrite(latchPin, HIGH);
 }
 
+void turnOnLED(){
+  switch (route[progress]){
+    case 'F': 
+      writeShiftRegister(B00001000);
+      break;
+
+    case 'L': 
+      writeShiftRegister(B00000100);
+      break;
+
+    case 'R': 
+      writeShiftRegister(B00000010);
+      break;
+
+    default:
+      writeShiftRegister(B00000001);
+  }
+}
+
 //function - returns the distance
 int getDistance() {
   //sends out a trigger sound
@@ -207,4 +242,40 @@ boolean checkObstacle(){
     return false;
   }
   return true;
+}
+
+String getMode(){
+  switch(currentMode){
+    case 0:
+      return "WOBBLEMODE";
+    case 1:
+      return "FORWARDMODE";
+    case 2:
+      return "LEFTMODE";
+    case 3:
+      return "RIGHTMODE";
+    case 4:
+      return "ENDOFROUTE";
+  }
+}
+
+void sensorDebug(){
+  Serial.println("sensorRight:");
+  Serial.println(sensorRight);
+  Serial.println("sensorLeft:");
+  Serial.println(sensorLeft);
+}
+
+void readSensors(){
+  sensorRight = analogRead(sensorR);
+  sensorLeft = analogRead(sensorL);
+}
+
+int readSensors(int sensorU){
+  if (sensorU == sensorRight){
+    return analogRead(sensorR);
+  } else if (sensorU == sensorLeft){
+    return analogRead(sensorL);
+  }
+  return 0;
 }
